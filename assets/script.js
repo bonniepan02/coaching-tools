dda97c79939c31883388c4b6100071bfea665d62
Bonnie Pan <bonniepan02@gmail.com>
2023-10-19 16:46:38 -0400

    trying to get markdown working

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,177 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let itemsData = JSON.parse(localStorage.getItem("itemsData")) || [
-        { name: "Powerful Questions", questions: "", status: false },
-        { name: "Metaphor", questions: "", status: false , status: false },
-        { name: "Bottom lining", questions: "", status: false , status: false },
-        { name: "Challenging", questions: "" , status: false },
-        { name: "Blurting(Intuition)", questions: "" , status: false },
-        { name: "Acknowledgement", questions: "" , status: false },
-        { name: "Articulating what's going on", questions: "" , status: false },
-        { name: "Championing", questions: "" , status: false },
-        { name: "Hold the Client's Agenda", questions: "" , status: false },
-        { name: "Holding the Focus", questions: "" , status: false },
-        { name: "Meta View", questions: "" , status: false },
-        { name: "Intrude", questions: "" , status: false },
-        { name: "Reframing", questions: "" , status: false },
-        { name: "Requesting", questions: "Will you..?" , status: false },
-        { name: "Take Charge", questions: "" , status: false },
-        { name: "Accountability", questions: "" , status: false },
-        { name: "Inquiry(to provoke reflection for learning)", questions: "" , status: false },
-        { name: "Asking Permission", questions: "" , status: false },
-    ];
+  let itemsData = JSON.parse(localStorage.getItem("itemsData")) || [
+    {
+      name: "Powerful Questions",
+      timing: "- to explore\n - to move to action",
+      goal: "to envoke thought, clarity, discovery, their learning or move them to action",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Metaphor",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Bottom lining",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Challenging",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Blurting(Intuition)",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Acknowledgement",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Articulating what's going on",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Championing",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Hold the Client's Agenda",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Holding the Focus",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    { name: "Meta View", timing: "", goal: "", questions: "", status: false },
+    { name: "Intrude", timing: "", goal: "", questions: "", status: false },
+    { name: "Reframing", timing: "", goal: "", questions: "", status: false },
+    {
+      name: "Requesting",
+      timing: "",
+      goal: "abc",
+      questions: "Will you do..?",
+      status: false,
+    },
+    {
+      name: "Take Charge",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Accountability",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Inquiry(to provoke reflection for learning)",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+    {
+      name: "Asking Permission",
+      timing: "",
+      goal: "",
+      questions: "",
+      status: false,
+    },
+  ];
 
-    // Get a reference to the table body
-    const tableBody = document.querySelector("#itemsTable tbody");
-
-    // Function to update the item's status in the data and Local Storage
-    function updateStatus(itemId, status) {
-        itemsData[itemId].status = status;
-        localStorage.setItem("itemsData", JSON.stringify(itemsData));
+  // Function to update the item's status in the data and Local Storage
+  function updateStatus(itemId, status) {
+    itemsData[itemId].status = status;
+    localStorage.setItem("itemsData", JSON.stringify(itemsData));
+  }
+  // Function to render Markdown into HTML
+  function renderMarkdown(text) {
+    if (typeof marked === "function") {
+      // Check if marked is available
+      return marked(text);
+    } else {
+      console.error("The 'marked' library is not available.");
+      return text; // Fallback to plain text
     }
+  }
+  // Get a reference to the table body
+  const tableBody = document.querySelector("#itemsTable tbody");
 
-    // Load or create initial data from Local Storage
-    const storedData = localStorage.getItem("itemsData");
-    if (!storedData) {
-        localStorage.setItem("itemsData", JSON.stringify(itemsData));
-    }
+  // Load or create initial data from Local Storage
+  if (Array.isArray(itemsData) && itemsData.length > 0) {
+    // The data is a non-empty array
+    // You can safely work with itemsData
+    console.log("Data is not an empty list:", itemsData);
 
     // Iterate through the data and create table rows
-    itemsData.forEach(function (item) {
-        const row = document.createElement("tr");
-        const itemNameCell = document.createElement("td");
-        const questionCell = document.createElement("td");
-        const statusCell = document.createElement("td");
+    itemsData.forEach(function (item, index) {
+      const row = document.createElement("tr");
+      const itemNameCell = document.createElement("td");
+      const timingCell = document.createElement("td");
+      const goalCell = document.createElement("td");
+      const questionCell = document.createElement("td");
+      const statusCell = document.createElement("td");
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = item.status;
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = item.status;
 
-        itemNameCell.textContent = item.name;
-        questionCell.textContent = item.questions;
-        statusCell.appendChild(checkbox);
+      itemNameCell.textContent = item.name;
+      timingCell.textContent = item.timing;
+      goalCell.textContent = item.goal;
+      questionCell.textContent = item.questions;
 
-        // Add event listener to checkbox for status updates
-        checkbox.addEventListener("change", function () {
-            updateStatus(index, checkbox.checked);
-        });
+      timingCell.innerHTML = renderMarkdown(item.timing);
+      // Add the data-render-markdown attribute
+      timingCell.setAttribute("data-render-markdown", "true");
 
-        row.appendChild(itemNameCell);
-        row.appendChild(questionCell);
-        row.appendChild(statusCell);
+      statusCell.appendChild(checkbox);
 
-        tableBody.appendChild(row);
+      // Add event listener to checkbox for status updates
+      checkbox.addEventListener("change", function () {
+        updateStatus(index, checkbox.checked);
+      });
+
+      row.appendChild(itemNameCell);
+      row.appendChild(timingCell);
+      row.appendChild(goalCell);
+      row.appendChild(questionCell);
+      row.appendChild(statusCell);
+
+      tableBody.appendChild(row);
     });
+  } else {
+    console.log("Data is empty or not a list.");
+  }
 });

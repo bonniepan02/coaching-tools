6067e8335f6323f54e9707f01fe01f3ca9ebff3b
Bonnie Pan <bonniepan02@gmail.com>
2023-10-19 14:41:55 -0400

    trying to reserve the status data using simplest approach

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const itemsData = [
-        { name: "Powerful Questions", questions: "" },
-        { name: "Metaphor", questions: "" },
-        { name: "Bottom lining", questions: "" },
-        { name: "Challenging", questions: "" },
-        { name: "Blurting(Intuition)", questions: "" },
-        { name: "Acknowledgement", questions: "" },
-        { name: "Articulating what's going on", questions: "" },
-        { name: "Championing", questions: "" },
-        { name: "Hold the Client's Agenda", questions: "" },
-        { name: "Holding the Focus", questions: "" },
-        { name: "Meta View", questions: "" },
-        { name: "Intrude", questions: "" },
-        { name: "Reframing", questions: "" },
-        { name: "Requesting", questions: "Will you..?" },
-        { name: "Take Charge", questions: "" },
-        { name: "Accountability", questions: "" },
-        { name: "Inquiry(to provoke reflection for learning)", questions: "" },
-        { name: "Asking Permission", questions: "" },
+    let itemsData = JSON.parse(localStorage.getItem("itemsData")) || [
+        { name: "Powerful Questions", questions: "", status: false },
+        { name: "Metaphor", questions: "", status: false , status: false },
+        { name: "Bottom lining", questions: "", status: false , status: false },
+        { name: "Challenging", questions: "" , status: false },
+        { name: "Blurting(Intuition)", questions: "" , status: false },
+        { name: "Acknowledgement", questions: "" , status: false },
+        { name: "Articulating what's going on", questions: "" , status: false },
+        { name: "Championing", questions: "" , status: false },
+        { name: "Hold the Client's Agenda", questions: "" , status: false },
+        { name: "Holding the Focus", questions: "" , status: false },
+        { name: "Meta View", questions: "" , status: false },
+        { name: "Intrude", questions: "" , status: false },
+        { name: "Reframing", questions: "" , status: false },
+        { name: "Requesting", questions: "Will you..?" , status: false },
+        { name: "Take Charge", questions: "" , status: false },
+        { name: "Accountability", questions: "" , status: false },
+        { name: "Inquiry(to provoke reflection for learning)", questions: "" , status: false },
+        { name: "Asking Permission", questions: "" , status: false },
     ];
 
     // Get a reference to the table body
     const tableBody = document.querySelector("#itemsTable tbody");
+
+    // Function to update the item's status in the data and Local Storage
+    function updateStatus(itemId, status) {
+        itemsData[itemId].status = status;
+        localStorage.setItem("itemsData", JSON.stringify(itemsData));
+    }
+
+    // Load or create initial data from Local Storage
+    const storedData = localStorage.getItem("itemsData");
+    if (!storedData) {
+        localStorage.setItem("itemsData", JSON.stringify(itemsData));
+    }
 
     // Iterate through the data and create table rows
     itemsData.forEach(function (item) {
@@ -32,11 +44,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.checked = false;
+        checkbox.checked = item.status;
 
         itemNameCell.textContent = item.name;
         questionCell.textContent = item.questions;
         statusCell.appendChild(checkbox);
+
+        // Add event listener to checkbox for status updates
+        checkbox.addEventListener("change", function () {
+            updateStatus(index, checkbox.checked);
+        });
 
         row.appendChild(itemNameCell);
         row.appendChild(questionCell);

6d1a00b96b0253612cac68310cd03be3f3dc6d43
Bonnie Pan <bonniepan02@gmail.com>
2023-10-24 14:42:35 -0400

    move the default data into its own data file

--- assets/script.js
+++ assets/script.js
@@ -1,179 +1,101 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let itemsData = JSON.parse(localStorage.getItem("itemsData")) || [
-    {
-      name: "Powerful Questions",
-      timing: " - to explore\n - to move to action",
-      goal: "- to envoke thought, clarity, discovery, or move them to action\n - invite coachees to look inside or into the future",
-      questions: "- What does that mean.../n - How...",
-      status: false,
-      tip: "- Listen and wait. Don't fill in the momentary silence\n",
-    },
-    {
-      name: "Metaphor",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Bottom lining",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Challenging",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Blurting(Intuition)",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Acknowledgement",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Articulating what's going on",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Championing",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Hold the Client's Agenda",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Holding the Focus",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    { name: "Meta View", timing: "", goal: "", questions: "", status: false },
-    { name: "Intrude", timing: "", goal: "", questions: "", status: false },
-    { name: "Reframing", timing: "", goal: "", questions: "", status: false },
-    {
-      name: "Requesting",
-      timing: "",
-      goal: "abc",
-      questions: "Will you do..?",
-      status: false,
-    },
-    {
-      name: "Take Charge",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Accountability",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Inquiry(to provoke reflection for learning)",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-    {
-      name: "Asking Permission",
-      timing: "",
-      goal: "",
-      questions: "",
-      status: false,
-    },
-  ];
+function renderMarkdown(text) {
+  if (typeof marked === "function") {
+    // Check if marked is available
+    return marked(text);
+  } else {
+    console.error("The 'marked' library is not available.");
+    return text; // Fallback to plain text
+  }
+}
 
+function updatePageContent(itemsData) {
   // Function to update the item's status in the data and Local Storage
   function updateStatus(itemId, status) {
     itemsData[itemId].status = status;
     localStorage.setItem("itemsData", JSON.stringify(itemsData));
   }
-  // Function to render Markdown into HTML
-  function renderMarkdown(text) {
-    if (typeof marked === "function") {
-      // Check if marked is available
-      return marked(text);
-    } else {
-      console.error("The 'marked' library is not available.");
-      return text; // Fallback to plain text
-    }
-  }
   // Get a reference to the table body
   const tableBody = document.querySelector("#itemsTable tbody");
+  // Iterate through the data and create table rows
+  itemsData.forEach(function (item, index) {
+    const row = document.createElement("tr");
+    const itemNameCell = document.createElement("td");
+    const timingCell = document.createElement("td");
+    const goalCell = document.createElement("td");
+    const questionCell = document.createElement("td");
+    const statusCell = document.createElement("td");
 
-  // Load or create initial data from Local Storage
-  if (Array.isArray(itemsData) && itemsData.length > 0) {
-    // The data is a non-empty array
-    // You can safely work with itemsData
-    console.log("Data is not an empty list:", itemsData);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.status;
 
-    // Iterate through the data and create table rows
-    itemsData.forEach(function (item, index) {
-      const row = document.createElement("tr");
-      const itemNameCell = document.createElement("td");
-      const timingCell = document.createElement("td");
-      const goalCell = document.createElement("td");
-      const questionCell = document.createElement("td");
-      const statusCell = document.createElement("td");
+    itemNameCell.textContent = item.name;
+    timingCell.textContent = item.timing;
+    goalCell.textContent = item.goal;
+    questionCell.textContent = item.questions;
 
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.checked = item.status;
+    timingCell.innerHTML = renderMarkdown(item.timing);
+    // Add the data-render-markdown attribute
+    timingCell.setAttribute("data-render-markdown", "true");
+    timingCell.classList.add("markdown-cell");
 
-      itemNameCell.textContent = item.name;
-      timingCell.textContent = item.timing;
-      goalCell.textContent = item.goal;
-      questionCell.textContent = item.questions;
+    statusCell.appendChild(checkbox);
 
-      timingCell.innerHTML = renderMarkdown(item.timing);
-      // Add the data-render-markdown attribute
-      timingCell.setAttribute("data-render-markdown", "true");
-      timingCell.classList.add("markdown-cell");
-
-      statusCell.appendChild(checkbox);
-
-      // Add event listener to checkbox for status updates
-      checkbox.addEventListener("change", function () {
-        updateStatus(index, checkbox.checked);
-      });
-
-      row.appendChild(itemNameCell);
-      row.appendChild(timingCell);
-      row.appendChild(goalCell);
-      row.appendChild(questionCell);
-      row.appendChild(statusCell);
-
-      tableBody.appendChild(row);
+    // Add event listener to checkbox for status updates
+    checkbox.addEventListener("change", function () {
+      updateStatus(index, checkbox.checked);
     });
+
+    row.appendChild(itemNameCell);
+    row.appendChild(timingCell);
+    row.appendChild(goalCell);
+    row.appendChild(questionCell);
+    row.appendChild(statusCell);
+
+    tableBody.appendChild(row);
+  });
+}
+async function fetchDefaultData() {
+  try {
+    const response = await fetch("assets/coaching-skills.json");
+    const defaultData = await response.json();
+    return defaultData;
+  } catch (error) {
+    console.error("Error fetching default data:", error);
+    return [];
+  }
+}
+
+(async () => {
+  const defaultData = await fetchDefaultData();
+  console.log("Loading page", defaultData);
+  let storedData = JSON.parse(localStorage.getItem("itemsData"));
+  let itemsData = [];
+
+  if (Array.isArray(storedData) && storedData.length > 0) {
+    itemsData = storedData;
+    console.log("Data is not empty", itemsData);
   } else {
     console.log("Data is empty or not a list.");
+    itemsData = defaultData;
+    localStorage.setItem("itemsData", JSON.stringify(itemsData));
+    console.log("Assigning data", itemsData);
   }
+  updatePageContent(itemsData);
+})();
+document.addEventListener("DOMContentLoaded", function () {
+  let storedData = JSON.parse(localStorage.getItem("itemsData"));
+  let itemsData = [];
+
+  if (Array.isArray(storedData) && storedData.length > 0) {
+    itemsData = storedData;
+    console.log("Data is not empty", itemsData);
+  } else {
+    console.log("Data is empty or not a list.");
+    itemsData = defaultData;
+    localStorage.setItem("itemsData", JSON.stringify(itemsData));
+    console.log("Assigning data", itemsData);
+  }
+
+  updatePageContent(itemsData);
 });
